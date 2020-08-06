52c3c5de93075eb17d48722ebebfa5fb9f565c6d
Raphael Araújo <raphaelaraujo075@gmail.com>
2020-08-06 17:19:56 -0300

    Tabela usuarios

---
+++ ordem/application/views/usuarios/index.php
@@ -0,0 +1,63 @@
+
+
+ 
+    <?php $this->load->view('layout/sidebar'); ?>
+
+<!-- Main Content -->
+<div id="content">
+
+ <?php $this->load->view('layout/navbar.php')?>
+  
+
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Library</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Data</li>
+            </ol>
+        </nav>
+
+          <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Usuario</th>
+                      <th>Login</th>
+                      <th>Ativo</th>
+                      <th>Ações</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php foreach($usuarios as $user): ?>
+                        <tr>
+                            <td><?php echo $user->id ?></td>
+                            <td><?php echo $user->username ?></td>
+                            <td><?php echo $user->email ?></td>
+                            <td><?php echo $user->active ?></td>
+                            <td>
+                                <a title="Editar" href="" class="btn btn-sm btn-primary">Editar</a>
+                                <a title="Excluir" href="" class="btn btn-sm btn-danger">Excluir</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+        </div>
+    </div>
+  <!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
