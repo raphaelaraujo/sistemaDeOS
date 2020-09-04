e0549b891822d828cfcea7c3a4732e3697b120de
Raphael Araújo <raphaelaraujo075@gmail.com>
2020-09-04 15:01:33 -0300

    Incluído o Módulo Vendedores até o edit.php

---
+++ bkp_banco/ordem_08.sql
@@ -0,0 +1,294 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 04-Set-2020 às 18:00
+-- Versão do servidor: 5.7.24
+-- versão do PHP: 7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ordem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `cliente_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cliente_data_cadastro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `cliente_tipo` tinyint(1) DEFAULT NULL,
+  `cliente_nome` varchar(45) NOT NULL,
+  `cliente_sobrenome` varchar(150) NOT NULL,
+  `cliente_data_nascimento` date NOT NULL,
+  `cliente_cpf_cnpj` varchar(20) NOT NULL,
+  `cliente_rg_ie` varchar(20) NOT NULL,
+  `cliente_email` varchar(50) NOT NULL,
+  `cliente_telefone` varchar(20) NOT NULL,
+  `cliente_celular` varchar(20) NOT NULL,
+  `cliente_cep` varchar(10) NOT NULL,
+  `cliente_endereco` varchar(155) NOT NULL,
+  `cliente_numero_endereco` varchar(20) NOT NULL,
+  `cliente_bairro` varchar(45) NOT NULL,
+  `cliente_complemento` varchar(145) NOT NULL,
+  `cliente_cidade` varchar(105) NOT NULL,
+  `cliente_estado` varchar(2) NOT NULL,
+  `cliente_ativo` tinyint(1) NOT NULL,
+  `cliente_obs` tinytext,
+  `cliente_data_alteracao` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`cliente_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`cliente_id`, `cliente_data_cadastro`, `cliente_tipo`, `cliente_nome`, `cliente_sobrenome`, `cliente_data_nascimento`, `cliente_cpf_cnpj`, `cliente_rg_ie`, `cliente_email`, `cliente_telefone`, `cliente_celular`, `cliente_cep`, `cliente_endereco`, `cliente_numero_endereco`, `cliente_bairro`, `cliente_complemento`, `cliente_cidade`, `cliente_estado`, `cliente_ativo`, `cliente_obs`, `cliente_data_alteracao`) VALUES
+(1, '2020-09-03 17:58:27', 1, 'Fabio Raphael', 'Araújo Furtado', '1993-05-17', '015.780.752-54', '4585369', 'raphaelaraujo075@gmail.com', '(91) 8064-7336', '(91) 98064-7336', '66640-445', 'Rua Chico Mendes (Res Carmelândia)', '17', 'Mangueirãi', 'Carmelândia', 'Belém', 'PA', 1, 'Teste de edit', '2020-09-03 17:58:39');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fornecedores`
+--
+
+DROP TABLE IF EXISTS `fornecedores`;
+CREATE TABLE IF NOT EXISTS `fornecedores` (
+  `fornecedor_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fornecedor_data_cadastro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `fornecedor_razao` varchar(200) DEFAULT NULL,
+  `fornecedor_nome_fantasia` varchar(145) DEFAULT NULL,
+  `fornecedor_cnpj` varchar(20) DEFAULT NULL,
+  `fornecedor_ie` varchar(20) DEFAULT NULL,
+  `fornecedor_telefone` varchar(20) DEFAULT NULL,
+  `fornecedor_celular` varchar(20) DEFAULT NULL,
+  `fornecedor_email` varchar(100) DEFAULT NULL,
+  `fornecedor_contato` varchar(45) DEFAULT NULL,
+  `fornecedor_cep` varchar(10) DEFAULT NULL,
+  `fornecedor_endereco` varchar(145) DEFAULT NULL,
+  `fornecedor_numero_endereco` varchar(20) DEFAULT NULL,
+  `fornecedor_bairro` varchar(45) DEFAULT NULL,
+  `fornecedor_complemento` varchar(45) DEFAULT NULL,
+  `fornecedor_cidade` varchar(45) DEFAULT NULL,
+  `fornecedor_estado` varchar(2) DEFAULT NULL,
+  `fornecedor_ativo` tinyint(1) DEFAULT NULL,
+  `fornecedor_obs` tinytext,
+  `fornecedor_data_alteracao` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`fornecedor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `fornecedores`
+--
+
+INSERT INTO `fornecedores` (`fornecedor_id`, `fornecedor_data_cadastro`, `fornecedor_razao`, `fornecedor_nome_fantasia`, `fornecedor_cnpj`, `fornecedor_ie`, `fornecedor_telefone`, `fornecedor_celular`, `fornecedor_email`, `fornecedor_contato`, `fornecedor_cep`, `fornecedor_endereco`, `fornecedor_numero_endereco`, `fornecedor_bairro`, `fornecedor_complemento`, `fornecedor_cidade`, `fornecedor_estado`, `fornecedor_ativo`, `fornecedor_obs`, `fornecedor_data_alteracao`) VALUES
+(1, '2020-09-03 15:50:02', 'Yra Tecnologia', 'Yra Inc - New', '55.467.821/0001-24', '809.307.365.959', '(91) 3279-2183', '(91) 98064-7336', 'yrainc@gmail.com', 'Fulano de Tal', '66640-445', 'Rua Chico Mendes', '17', 'Mangueirão', 'Conj. Carmelândia', 'Belém', 'PA', 1, 'Fornecedor vende produtos de tecnologia voltadas para desenvolvimento web', '2020-09-03 19:14:02');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `description`) VALUES
+(1, 'admin', 'Administrator'),
+(2, 'vendedor', 'Vendedor');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `login_attempts`
+--
+
+DROP TABLE IF EXISTS `login_attempts`;
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(45) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `time` int(11) UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sistema`
+--
+
+DROP TABLE IF EXISTS `sistema`;
+CREATE TABLE IF NOT EXISTS `sistema` (
+  `sistema_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sistema_razao_social` varchar(145) DEFAULT NULL,
+  `sistema_nome_fantasia` varchar(145) DEFAULT NULL,
+  `sistema_cnpj` varchar(25) DEFAULT NULL,
+  `sistema_ie` varchar(25) DEFAULT NULL,
+  `sistema_telefone_fixo` varchar(25) DEFAULT NULL,
+  `sistema_telefone_movel` varchar(25) NOT NULL,
+  `sistema_email` varchar(100) DEFAULT NULL,
+  `sistema_site_url` varchar(100) DEFAULT NULL,
+  `sistema_cep` varchar(25) DEFAULT NULL,
+  `sistema_endereco` varchar(145) DEFAULT NULL,
+  `sistema_numero` varchar(25) DEFAULT NULL,
+  `sistema_cidade` varchar(45) DEFAULT NULL,
+  `sistema_estado` varchar(2) DEFAULT NULL,
+  `sistema_txt_ordem_servico` tinytext,
+  `sistema_data_alteracao` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sistema_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `sistema`
+--
+
+INSERT INTO `sistema` (`sistema_id`, `sistema_razao_social`, `sistema_nome_fantasia`, `sistema_cnpj`, `sistema_ie`, `sistema_telefone_fixo`, `sistema_telefone_movel`, `sistema_email`, `sistema_site_url`, `sistema_cep`, `sistema_endereco`, `sistema_numero`, `sistema_cidade`, `sistema_estado`, `sistema_txt_ordem_servico`, `sistema_data_alteracao`) VALUES
+(1, 'System Ordem Inc.', 'Sistema de Ordem Now', '45.825.545/4554-55', '15-469019-8', '(91) 99999-9999', '(55) 55555-5555', 'ordemnow@contato.com.br', 'http://localhost/ordem/', '66666-666', 'Rua Oswaldo Cruz', '600', 'BELEM', 'PA', 'Nossa empresa é a sua marca', '2020-08-31 22:51:55');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(45) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `activation_selector` varchar(255) DEFAULT NULL,
+  `activation_code` varchar(255) DEFAULT NULL,
+  `forgotten_password_selector` varchar(255) DEFAULT NULL,
+  `forgotten_password_code` varchar(255) DEFAULT NULL,
+  `forgotten_password_time` int(11) UNSIGNED DEFAULT NULL,
+  `remember_selector` varchar(255) DEFAULT NULL,
+  `remember_code` varchar(255) DEFAULT NULL,
+  `created_on` int(11) UNSIGNED NOT NULL,
+  `last_login` int(11) UNSIGNED DEFAULT NULL,
+  `active` tinyint(1) UNSIGNED DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uc_email` (`email`),
+  UNIQUE KEY `uc_activation_selector` (`activation_selector`),
+  UNIQUE KEY `uc_forgotten_password_selector` (`forgotten_password_selector`),
+  UNIQUE KEY `uc_remember_selector` (`remember_selector`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `email`, `activation_selector`, `activation_code`, `forgotten_password_selector`, `forgotten_password_code`, `forgotten_password_time`, `remember_selector`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
+(1, '127.0.0.1', 'administrator', '$2y$12$hfjpKjYjXx0qTa9T96vFmuA05eJV6c3aPwzHOR/vsJ7T893fNhCOq', 'admin@admin.com', NULL, '', NULL, NULL, NULL, NULL, NULL, 1268889823, 1599218930, 1, 'Admin', 'istrator', 'ADMIN', '0'),
+(2, '::1', 'raphaelaraujo075', '$2y$12$j6Vvc6lKFbA6ht8JYc7PWOzLBtGTvRwhqMfpnr34.bCjzVQp52Zce', 'raphaelaraujo075@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1597676523, 1599146846, 1, 'Raphael', 'Furtado', NULL, NULL),
+(3, '::1', 'yasminpassinho', '$2y$10$s6FXEsNyppM.JEMOZ5fy..MXDHxeVZ5E7X6ifYWTX7dPNwrPCuHPe', 'yasminpassinho@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1597764478, 1598914524, 1, 'Yasmin', 'Furtado', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users_groups`
+--
+
+DROP TABLE IF EXISTS `users_groups`;
+CREATE TABLE IF NOT EXISTS `users_groups` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `group_id` mediumint(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  KEY `fk_users_groups_users1_idx` (`user_id`),
+  KEY `fk_users_groups_groups1_idx` (`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `users_groups`
+--
+
+INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
+(4, 1, 1),
+(10, 2, 1),
+(11, 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `vendedores`
+--
+
+DROP TABLE IF EXISTS `vendedores`;
+CREATE TABLE IF NOT EXISTS `vendedores` (
+  `vendedor_id` int(11) NOT NULL AUTO_INCREMENT,
+  `vendedor_codigo` varchar(10) NOT NULL,
+  `vendedor_data_cadastro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `vendedor_nome_completo` varchar(145) NOT NULL,
+  `vendedor_cpf` varchar(25) NOT NULL,
+  `vendedor_rg` varchar(25) NOT NULL,
+  `vendedor_telefone` varchar(15) DEFAULT NULL,
+  `vendedor_celular` varchar(15) DEFAULT NULL,
+  `vendedor_email` varchar(45) DEFAULT NULL,
+  `vendedor_cep` varchar(15) DEFAULT NULL,
+  `vendedor_endereco` varchar(45) DEFAULT NULL,
+  `vendedor_numero_endereco` varchar(25) DEFAULT NULL,
+  `vendedor_complemento` varchar(45) DEFAULT NULL,
+  `vendedor_bairro` varchar(45) DEFAULT NULL,
+  `vendedor_cidade` varchar(45) DEFAULT NULL,
+  `vendedor_estado` varchar(2) DEFAULT NULL,
+  `vendedor_ativo` tinyint(1) DEFAULT NULL,
+  `vendedor_obs` tinytext,
+  `vendedor_data_alteracao` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`vendedor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `vendedores`
+--
+
+INSERT INTO `vendedores` (`vendedor_id`, `vendedor_codigo`, `vendedor_data_cadastro`, `vendedor_nome_completo`, `vendedor_cpf`, `vendedor_rg`, `vendedor_telefone`, `vendedor_celular`, `vendedor_email`, `vendedor_cep`, `vendedor_endereco`, `vendedor_numero_endereco`, `vendedor_complemento`, `vendedor_bairro`, `vendedor_cidade`, `vendedor_estado`, `vendedor_ativo`, `vendedor_obs`, `vendedor_data_alteracao`) VALUES
+(1, '09842571', '2020-01-28 01:24:17', 'Lucio Antonio de Souza - TESTE', '946.873.070-00', '36.803.319-3', '(41) 3999-9999', '(41) 99999-9999', 'vendedor@gmail.com', '80530-000', 'Rua das vendas', '45', '', 'Centro', 'Curitiba', 'PR', 0, '', '2020-09-04 14:08:20'),
+(2, '03841956', '2020-01-29 22:22:27', 'Sara Betina', '582.071.790-23', '25.287.429-8', '(41) 3884-4444', '(41) 88884-4444', 'sara@gmail.com', '80530-000', 'Rua das vendas', '45', '', 'Centro', 'Joinville', 'SC', 0, '', '2020-09-04 14:06:11');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
