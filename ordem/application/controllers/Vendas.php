87268efc0b9e5084e72e91766beeb9d79fca175b
Raphael Araújo <raphaelaraujo075@gmail.com>
2021-03-22 20:45:46 -0300

    Começando módulo de Vendas

---
+++ ordem/application/controllers/Vendas.php
@@ -0,0 +1,45 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Vendas extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+
+        if (!$this->ion_auth->logged_in()) {
+            $this->session->set_flashdata('info', 'Sua sessão expirou!');
+            redirect('login');
+        }
+
+        $this->load->model('vendas_model');
+    }
+
+    public function index() {
+
+        $data = array(
+            'titulo' => 'Vendas cadastradas',
+            'styles' => array(
+                'vendor/datatables/dataTables.bootstrap4.min.css'
+            ),
+            'scripts' => array(
+                'vendor/datatables/jquery.dataTables.min.js',
+                'vendor/datatables/dataTables.bootstrap4.min.js',
+                'vendor/datatables/app.js'
+            ),
+            //'vendas' => $this->ordem_servicos_model->get_all(),
+            'vendas' => $this->vendas_model->get_all(),
+        );
+
+        /*
+          echo '<pre>';
+          print_r($data['vendas']);
+          exit();
+        */ 
+
+        $this->load->view('layout/header', $data);
+        $this->load->view('vendas/index');
+        $this->load->view('layout/footer');
+    }
+
+}
---
+++ ordem/application/views/vendas/imprimir.php
@@ -0,0 +1,58 @@
+<?php $this->load->view('layout/sidebar'); ?>
+
+<!-- Main Content -->
+<div id="content">
+
+    <?php $this->load->view('layout/navbar.php') ?>
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?php echo base_url('os') ?>">Ordens de serviço</a></li>
+                <li class="breadcrumb-item active" aria-current="page"><?php echo $titulo ?></li>
+            </ol>
+        </nav>
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <a href="<?php echo base_url('os/pdf') ?>" class="btn btn-dark btn-icon-split btn-lg">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-print"></i>
+                            </span>
+                            <span class="text">Imprimir ordem de serviço</span>
+                        </a>                        
+                    </div>
+
+                    <div class="col-md-4">
+                        <a href="<?php echo base_url('os/add') ?>" class="btn btn-success btn-icon-split btn-lg">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-plus"></i>
+                            </span>
+                            <span class="text">Nova ordem de serviço</span>
+                        </a>                        
+                    </div>
+
+                    <div class="col-md-4">
+                        <a href="<?php echo base_url('os') ?>" class="btn btn-info btn-icon-split btn-lg">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-list-ol"></i>
+                            </span>
+                            <span class="text">Listar ordem de serviço</span>
+                        </a>                        
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
